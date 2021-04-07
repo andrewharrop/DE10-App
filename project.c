f14f0c2db03cce8e476a2b6d7fe026cf172d650e
matthew-vidovic <65307667+matthew-vidovic@users.noreply.github.com>
2021-04-06 21:41:56 -0400

    Update project.c

--- project.c
+++ project.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "address_map_arm.h"
 
 // Due to the nature of the current circumstances, peripherals could be used to simulate the real world application
 //Jac
